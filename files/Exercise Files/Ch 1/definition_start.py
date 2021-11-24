15c0f7e8ab4001b4547647df610c19586e6861f0
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-24 12:20:19 +0530

    defition excercise done

--- files/Exercise Files/Ch 1/definition_start.py
+++ files/Exercise Files/Ch 1/definition_start.py
@@ -3,9 +3,15 @@
 
 
 # TODO: create a basic class
+class Book:
+    # this is intializer function not a constructor
+    def __init__(self, title):
+        self.title = title
 
 
 # TODO: create instances of the class
-
+b1 = Book('My book title')
 
 # TODO: print the class and property
+print(b1)
+print(b1.title)
